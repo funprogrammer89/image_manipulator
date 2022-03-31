fe83ccb3e56c78851bf3d8fed1190251d706e42d
Ken Elliott <Kennethpelliott@outlook.com>
2022-03-31 00:21:25 -0400

    Added Shrink function and fixed Rotate left function to actually rotate left and added a Right rotation option which just rotates left three times.

--- src/Editor.java
+++ src/Editor.java
@@ -1,102 +1,101 @@
 import java.awt.Color;
 import java.util.*;
-public class Editor {
 
+public class Editor {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.print("What picture would you like to edit? ");
         Scanner in = new Scanner(System.in);
         String file = in.next();
         Picture picture = new Picture(file);
         int choice = 0;
-        while (choice != 6) {
-            System.out.println("");
-            System.out.println("Operations");
-            System.out.println("1. Grow");
-            System.out.println("2. Grayscale");
-            System.out.println("3. Invert");
-            System.out.println("4. Rotate Left");
-            System.out.println("5. Display Image");
-            System.out.println("6. Quit");
+        while (choice != 8) {
+            System.out.println("\nOperations");
+            System.out.println("1. Shrink");
+            System.out.println("2. Grow");
+            System.out.println("3. Grayscale");
+            System.out.println("4. Invert");
+            System.out.println("5. Rotate Right");
+            System.out.println("6. Rotate Left");
+            System.out.println("7. Display Image");
+            System.out.println("8. Quit");
+
             System.out.print("Enter command: ");
             choice = in.nextInt();
             if (choice == 1) {
+                picture = shrink(picture);
+            } else if (choice == 2) {
                 picture = grow(picture);
-            }
-            else if (choice == 2) {
+            } else if (choice == 3) {
                 picture = grayscale(picture);
-            }
-            else if (choice == 3) {
+            } else if (choice == 4) {
                 picture = invert(picture);
-            }
-            else if (choice == 4) {
-                picture = rotateLeft(picture);
-            }
-            else if (choice == 5) {
+            } else if (choice == 5) {
+                picture = rotateRight(picture);
+            } else if (choice == 6) { // Three right rotations = left rotation
+                picture = rotateRight(picture);
+                picture = rotateRight(picture);
+                picture = rotateRight(picture);
+            } else if (choice == 7) {
                 picture.show();
             }
         }
-    }
 
-    public static Picture grow( Picture p ) {
-
+}
+    public static Picture grow(Picture p) {
         Picture growPicture = new Picture(p.width() * 2, p.height() * 2);
-
-
         for (int i = 0; i < growPicture.width(); i++) {
             for (int j = 0; j < growPicture.height(); j++) {
-                Color pixel = p.get(i/2, j/2);
+                Color pixel = p.get(i / 2, j / 2);
                 growPicture.set(i, j, pixel);
-
-
             }
-
         }
-
-
         return growPicture;
     }
 
+    public static Picture shrink(Picture p) {
+        Picture shrinkPicture = new Picture(p.width() / 2, p.height() / 2);
+        for (int i = 0; i < shrinkPicture.width(); i++) {
+            for (int j = 0; j < shrinkPicture.height(); j++) {
+                Color pixel = p.get(i * 2, j * 2);
+                shrinkPicture.set(i, j, pixel);
+            }
+        }
+        return shrinkPicture;
+    }
 
-    public static Picture invert( Picture p ) {
-        Picture invertPicture = new Picture(p.width(),p.height());
+    public static Picture invert(Picture p) {
+        Picture invertPicture = new Picture(p.width(), p.height());
         for (int i = 0; i < p.width(); ++i) {
             for (int j = 0; j < p.height(); ++j) {
                 Color pixel = p.get(i, j);
-                Color newPixel = new Color(255 - pixel.getRed(),255 - pixel.getGreen(),255 - pixel.getBlue());
+                Color newPixel = new Color(255 - pixel.getRed(), 255 - pixel.getGreen(), 255 - pixel.getBlue());
                 invertPicture.set(i, j, newPixel);
             }
         }
         return invertPicture;
     }
 
-    public static Picture grayscale( Picture p ) {
-        Picture grayscalePicture = new Picture(p.width(),p.height());
+    public static Picture grayscale(Picture p) {
+        Picture grayscalePicture = new Picture(p.width(), p.height());
         for (int i = 0; i < p.width(); ++i) {
             for (int j = 0; j < p.height(); ++j) {
                 Color pixel = p.get(i, j);
-                int greyscaleValue = (int)Math.round((.3 * pixel.getRed() + .59 * pixel.getGreen() + .11 * pixel.getBlue()));
-                Color newPixel = new Color(greyscaleValue,greyscaleValue,greyscaleValue);
+                int greyscaleValue = (int) Math.round((.3 * pixel.getRed() + .59 * pixel.getGreen() + .11 * pixel.getBlue()));
+                Color newPixel = new Color(greyscaleValue, greyscaleValue, greyscaleValue);
                 grayscalePicture.set(i, j, newPixel);
             }
         }
-
         return grayscalePicture;
     }
 
-
-    public static Picture rotateLeft( Picture p ) {
-        Picture rotateLeftPicture = new Picture(p.height(),p.width());
+    public static Picture rotateRight(Picture p) {
+        Picture rotateRightPicture = new Picture(p.height(), p.width());
 
         for (int i = 0; i < p.width(); ++i) {
             for (int j = 0; j < p.height(); ++j) {
-                //Color pixel = p.get(i, j);
-                rotateLeftPicture.set(p.height()-j-1,i, p.get(i, j));
+                rotateRightPicture.set(p.height() - j - 1, i, p.get(i, j));
             }
         }
-        return rotateLeftPicture;
-    }
-    public static void sort( int[] values ) {
-
+        return rotateRightPicture;
     }
 }
